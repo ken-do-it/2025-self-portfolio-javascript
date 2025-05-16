e902098e44bf9fe4c4221ca13e30e053ba9be755
kijin lee <lqizhen@naver.com>
2025-05-16 20:22:05 +0900

    chang url to online

--- public/main.js
+++ public/main.js
@@ -32,13 +32,13 @@ const formatDateWithDay = (datetimeStr) => {
 const getActives = async () => {
 
 // --- 배포할때는 아래 2개 주석처리  이부분은 로컬용 
-    let url = new URL(`http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000/`)
-    const response = await fetch(url)
+    // let url = new URL(`http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000/`)
+    // const response = await fetch(url)
 //-----------
 
 
 //-------------------------- 이 아래 부분 vercel 배포 시 주석 해제 
-//   const response = await fetch('/api/getEvents');  
+  const response = await fetch('/api/getEvents');  
 //-----------------------------------
   const data = await response.json();
 
